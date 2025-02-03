655dc2bdd1280cedd021103153789b2bb7c42e8e
Himanshu Tyagi <himnhcs@gmail.com>
2025-02-03 07:59:49 +0000

    after update

--- node-express/scripts/install_dependencies.sh
+++ node-express/scripts/install_dependencies.sh
@@ -2,14 +2,16 @@
 cd /home/ubuntu/nodejs-app || exit
 
 echo "Installing Node.js and npm..."
-
-# Update package list and install Node.js
 sudo apt-get update -y
 sudo apt-get install -y nodejs npm
 
 # Verify Node.js and npm installation
 node -v
 npm -v
+
+echo "Fixing permissions for the application directory..."
+# Ensure the current user has ownership of the application directory
+sudo chown -R ubuntu:ubuntu /home/ubuntu/nodejs-app
 
 echo "Installing dependencies..."
 npm install
