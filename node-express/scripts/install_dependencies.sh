3d9e249f4c28f077d88e687057a4e9fb39c1f7d8
Himanshu Tyagi <himnhcs@gmail.com>
2025-02-03 07:31:36 +0000

    updated

--- node-express/scripts/install_dependencies.sh
+++ node-express/scripts/install_dependencies.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 cd /home/ubuntu/nodejs-app || exit
+
+echo "Installing Node.js and npm..."
+
+# Update package list and install Node.js
+sudo apt-get update -y
+sudo apt-get install -y nodejs npm
+
+# Verify Node.js and npm installation
+node -v
+npm -v
+
 echo "Installing dependencies..."
 npm install
+
 
