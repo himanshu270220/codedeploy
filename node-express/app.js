e6b0fc55789441531a2aec562088c26275e44617
Himanshu Tyagi <himnhcs@gmail.com>
2025-02-04 09:31:55 +0000

    first look

--- node-express/app.js
+++ node-express/app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World! This is deployed using AWS CodeDeploy.");
+  res.end("Hello, World! This is deployed using AWS CodeDeployis have.");
 });
 
 server.listen(port, hostname, () => {
